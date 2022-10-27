a47a59c1ba1ed444a7e0b1cf6b9ae3d368357b86
Lemuel Manske <lemuelkaue@gmail.com>
2022-10-27 15:58:50 -0300

    Atualizando e criando novas rotas
    despesa e receita, ainda não finalizado

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,12 +1,12 @@
-from http.client import CONFLICT
+from http.client import CONFLICT, NOT_FOUND, UNAUTHORIZED
 
 from flask import abort, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from app import app, db
-from app.models import ContaBancaria
-from app.utils import get_validate
-from app.utils.choices import Moedas, Instituicoes
+from app.models import ContaBancaria, Transacao
+from app.utils import get_conta_bancaria, get_validate
+from app.utils.choices import Moedas, Instituicoes, TipoTransacao
 
 
 @app.route('/home', methods=['GET'])
@@ -19,11 +19,16 @@ def home_route():
 
 @app.route('/criar/conta-bancaria', methods=['POST'])
 @jwt_required()
-def create_bank_account_route():
+def rota_criar_conta_bancaria():
+	'''
+	TODO
+	'''
 	moeda, saldo, instituicao = get_validate(request.get_json(), 
-	{'moeda': str,
-	 'saldo': str,
-	 'instituicao': str})
+	{
+		'moeda': str,
+		'saldo': str,
+		'instituicao': str
+	})
 
 	id_usuario = get_jwt_identity()
 
@@ -42,3 +47,36 @@ def create_bank_account_route():
 	db.session.commit()
 
 	return jsonify(nova_conta_bancaria.json()), 200
+
+
+@app.route('/criar/<tipo: str>', methods=['POST'])
+@app.route('/criar/despesa', methods=['POST'])
+def rota_criar_despesa():
+	'''
+	TODO
+	'''
+	valor, descricao, resolvido, id_categoria, id_conta_bancaria = \
+		get_validate(request.get_json(),
+	{
+		'valor': str, 
+		'descricao': str, 
+		'resolvido': bool, 
+		'id_categoria': int, 
+		'id_conta_bancaria': int
+	})
+
+	id_usuario = get_jwt_identity()
+
+	conta_bancaria = get_conta_bancaria(id_usuario, id_conta_bancaria)
+
+	nova_despesa = Transacao(
+		tipo = TipoTransacao.DESPESA,
+		valor = valor,
+		descricao = descricao,
+		resolvido = resolvido,
+		id_categoria = id_categoria,
+		id_conta_bancaria = id_conta_bancaria,
+	)
+
+	db.session.add(nova_despesa)
+	db.session.commit
