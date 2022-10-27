68f41bd5c8e7e21297d34f267215a7c95770ef1d
Lemuel Manske <lemuelkaue@gmail.com>
2022-10-27 15:59:12 -0300

    Atualizando utils
    Verificação de conta bancária

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,11 +1,10 @@
-from http.client import BAD_REQUEST
-
-from enum import Enum
+from http.client import BAD_REQUEST, NOT_FOUND
 
 from operator import itemgetter
 
 from flask import abort
 
+from app.models import ContaBancaria
 
 def get_validate(data: any, schema: 'dict[str, type]') -> 'dict[str, any]':
 
@@ -14,6 +13,15 @@ def get_validate(data: any, schema: 'dict[str, type]') -> 'dict[str, any]':
 
 	for key in schema:
 		if (key not in data) or (type(data[key]) != schema[key]):
-			abort(BAD_REQUEST)
+			abort(BAD_REQUEST) # 401
 
 	return itemgetter(*schema)(data)
+
+
+def get_conta_bancaria(b_id: int, u_id: int):
+	c = ContaBancaria.query.filter_by(id=b_id, id_usuario=u_id)
+	
+	if not c:
+		abort(NOT_FOUND)
+
+	return c
