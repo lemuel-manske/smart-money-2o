8209ee3d318286cff633f6e207e134e6512e3c1e
Lemuel Manske <lemuelkaue@gmail.com>
2022-10-30 22:40:42 -0300

    Ajustando rotas
    - Novo gerenciador de erros http;
    - Ajustando type annotations;
    - Ajustando documentação;
    - Adicionando rota de alterar conta.

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,7 @@
-from http.client import CONFLICT, NOT_FOUND, UNAUTHORIZED
-
 from flask import abort, jsonify, request
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
-from app import TOKEN_UPDATE_HEADER, app, db, bcrypt
+from app import TOKEN_UPDATE_HEADER, app, bcrypt
 from app.models import Usuario
 from app.utils import get_validate
 
@@ -14,33 +12,35 @@ def rota_login():
 	Realiza o login do usuário via POST.
 
 	Realiza request de dados em json (email e senha), 
-	validando os mesmos (get_validade).
+	validando os mesmos (`get_validade`).
 
 	Returns:
 		CÓD. 200 (OK): Criação de JWT (identidade id) e 
 			retorna objeto usuário em formato json.
-		CÓD. 401 (UNAUTHORIZED): senha incorreta.
-		CÓD. 404 (NOT_FOUND): usuário não encontrado 
+		CÓD. 401 (UNAUTHORIZED): Senha incorreta.
+		CÓD. 404 (NOT_FOUND): Usuário não encontrado 
 			no banco de dados.
 	'''
 	email, senha = get_validate(request.get_json(),
-		{'email': str, 
-		 'senha':str})
+		{
+			'email': str, 
+			'senha':str
+		})
 
-	usuario = Usuario.query.filter_by(email=email).first()
+	usuario: Usuario = Usuario.query.filter_by(email=email).first()
 
-	if (usuario == None):
-		abort(NOT_FOUND)
+	if usuario == None:
+		return jsonify('Usuário não encontrado.'), 404
 
 	if not bcrypt.check_password_hash(usuario.senha, senha):
-		abort(UNAUTHORIZED)
+		return jsonify('Senha incorreta.'), 409
 	
 	tk = create_access_token(identity=usuario.id)
 
-	response = jsonify(usuario.json())
-	response.headers.set(TOKEN_UPDATE_HEADER, tk)
+	res = jsonify(usuario.json())
+	res.headers.set(TOKEN_UPDATE_HEADER, tk)
 
-	return response, 200
+	return res, 200
 	
 
 @app.route('/cadastro', methods=['POST'])
@@ -49,7 +49,7 @@ def rota_register():
 	Realiza registro de um novo usuário via POST.
 
 	Realiza request de dados em json (email, senha e nome),
-	validando os mesmos (get_validate).
+	validando os mesmos (`get_validate`).
 
 	Realiza o login imediatamente após confirmação de sucesso (JWT).
 
@@ -59,23 +59,22 @@ def rota_register():
 		CÓD. 409 (CONFLICT): Usuário já existe (filtro p/ email).
 	'''
 	email, senha, nome = get_validate(request.get_json(), \
-		{'email': str,
-		 'senha': str,
-		 'nome': str})
+		{
+			'email': str,
+			'senha': str,
+			'nome': str
+		})
 
 	if Usuario.query.filter_by(email=email).first() != None:
-		abort(CONFLICT)
+		return jsonify('Email já cadastrado.'), 409
 		
 	senha_hash = bcrypt.generate_password_hash(senha).decode('UTF-8')
 
-	novo_usuario = Usuario(
+	novo_usuario: Usuario = Usuario.create(
 		email=email,
 		senha=senha_hash,
 		nome=nome
 	)
-
-	db.session.add(novo_usuario)
-	db.session.commit()
 
 	tk = create_access_token(identity=novo_usuario.id)
 
@@ -85,10 +84,47 @@ def rota_register():
 	return res, 200
 
 
+@app.route('/atualizar-conta', methods=['POST'])
+@jwt_required()
+def rota_atualizar_conta():
+	email, senha, senha_nova, nome = get_validate(request.get_json(), \
+		{
+			'email': str,
+			'senha': str,
+			'nova_senha': str,
+			'nome': str
+		})
+	
+	id_usuario = get_jwt_identity()
+
+	usuario: Usuario = Usuario.query.filter_by(id=id_usuario).first()
+
+	if not bcrypt.check_password_hash(usuario.senha, senha):
+		return jsonify('Senha incorreta.'), 409
+
+	nova_senha_hash = bcrypt.generate_password_hash(senha_nova).decode('UTF-8')
+
+	usuario.update(
+		email=email, 
+		senha=nova_senha_hash,
+		nome=nome
+	)
+
+	return jsonify(usuario.json()), 200
+
+
 @app.route('/minha-conta', methods=['GET'])
 @jwt_required()
-def rota_account():	
-	current_user_id = get_jwt_identity()
-	curr_user = Usuario.query.filter_by(id=current_user_id).first()
+def rota_account():
+	'''
+	Retorna os dados da conta do usuário com login na sessão.
+
+	Returns:
+		CÓD. 200 (OK): Retorno dos dados do usuário em
+			formato json.
+	'''
+	id_usuario = get_jwt_identity()
+
+	usuario: Usuario = Usuario.query.filter_by(id=id_usuario).first()
 	
-	return jsonify(curr_user.json()), 200
+	return jsonify(usuario.json()), 200
