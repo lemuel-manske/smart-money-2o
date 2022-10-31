be70a1c20fcb48cfbb850b7f6f8cba9232dd59f5
Lemuel Manske <lemuelkaue@gmail.com>
2022-10-30 22:39:16 -0300

    Atualizando modelos
    - Adicionando type annotations;
    - Resolvendo processamento de transações (do_transacao);
    - Alterando valores de colunas;
    - Criando modelo base para criação e edição de dados por herança.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,17 @@
-from calendar import month
-from datetime import timedelta
 from enum import Enum
+from typing import Type, TypeVar
 
 from sqlalchemy.sql import func
+from sqlalchemy import inspect
 
 from app import db
 from app.utils.choices import Instituicoes, Moedas, TipoTransacao
+from app.utils import do_add_commit
 
 
-def get_val(self, field):
+T = TypeVar('T')
+
+def get_val(self, field: str) -> str:
 	v = self.__getattribute__(field)
 
 	if issubclass(type(v), Enum):
@@ -16,12 +19,51 @@ def get_val(self, field):
 	else:
 		return v
 
-def to_json(*fields):
-	return lambda self: { 
-		field: get_val(self, field) for field in fields 
+def to_json(*fields) -> dict:
+	return lambda self: {
+		field: get_val(self, field) for field in fields
 	}
 
-class Usuario(db.Model):
+class BaseModelClass():
+	@classmethod
+	def create(cls: Type[T], **kwargs) -> T:
+		'''
+		Realiza a criação de instância de modelo (´db.Model´).
+		Adiciona e salva as alterações no banco de dados 
+		automaticamente (`do_add_commit`).
+
+		Args:
+			kwargs: Dicionário contendo dados necessários.
+
+		Returns:
+			inst: Instância de modelo criada
+		'''
+		inst = cls(**kwargs)
+		do_add_commit(inst)
+
+		return inst
+	
+	def update(self, **kwargs) -> None:
+		'''
+		Realiza o update dos atributos de uma instancia de 
+		modelo (`db.Model`). Salva as alterações automaticamente
+		no banco de dados.
+		
+		Args:
+			kwargs: Dicionário contendo novos dados.
+		
+		Returns:
+			None.
+		'''
+		attributes = inspect(self).attrs
+		for attr in attributes:
+			if attr.key in kwargs and attr.key != 'id':
+				self.__setattr__(attr.key, kwargs[attr.key])
+			
+		db.session.commit()
+		
+
+class Usuario(BaseModelClass, db.Model):
 	__tablename__ = 'usuario'
 
 	id = db.Column(db.Integer, primary_key=True)
@@ -30,16 +72,19 @@ class Usuario(db.Model):
 	senha = db.Column(db.Text, nullable=False)
 	premium = db.Column(db.Boolean, nullable=False, default=False) # True | False
 
-	contas_bancarias = db.relationship('ContaBancaria', backref='usuario', lazy='select')
-
 	json = to_json('id', 'nome', 'email', 'senha', 'premium')	
 
-	def __str__(self):
+
+	contas_bancarias = db.relationship('ContaBancaria', backref='usuario', lazy='select')
+	categorias = db.relationship('Categoria', backref='usuario', lazy='select')
+	transacoes = db.relationship('Transacao', backref='usuario', lazy='select')
+
+	def __str__(self) -> str:
 		return f'<Usuario: id:{self.id}, nome:{self.name}, email:{self.email}, \
 			senha:{self.password}, premium:{self.premium}>'
 
 
-class ContaBancaria(db.Model):
+class ContaBancaria(BaseModelClass, db.Model):
 	__tablename__ = 'conta_bancaria'
 
 	id = db.Column(db.Integer, primary_key=True)
@@ -47,50 +92,64 @@ class ContaBancaria(db.Model):
 	saldo = db.Column(db.Numeric, nullable=False)
 	instituicao = db.Column(db.Enum(Instituicoes), nullable=False)
 
+	json = to_json('id', 'moeda', 'saldo', 'instituicao', 'id_usuario')
+
 	id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 
 	despesas = db.relationship('Transacao', backref='conta_bancaria', lazy='select')
 
-	json = to_json('id', 'moeda', 'saldo', 'instituicao', 'id_usuario')
-
-	def __str__(self):
+	def __str__(self) -> str:
 		return f'<ContaBancaria: id:{self.id}, moeda:{self.moeda}, \
-			saldo:{self.saldo}, instituicao:{self.instituicao}, id_usuario:{self.id_usuario}>'
+			saldo:{self.saldo}, instituicao:{self.instituicao}, \
+				id_usuario:{self.id_usuario}>'
 
-class Transacao(db.Model):
+class Transacao(BaseModelClass, db.Model):
 	__tablename__ = 'transacao'
 
 	id = db.Column(db.Integer, primary_key=True)
 	tipo = db.Column(db.Enum(TipoTransacao), nullable=False) # despesa | receita
 	valor = db.Column(db.Numeric, nullable=False)
 	descricao = db.Column(db.Text, nullable=False)
-	resolvido = db.Column(db.Boolean, nullable=False, default=False) # pago (despesa) x recebido (receita)
-	data_origem = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.utcnow())
-
-	id_categoria = db.Column(db.Integer, db.ForeignKey('categoria.id'))
-	id_conta_bancaria = db.Column(db.Integer, db.ForeignKey('conta_bancaria.id'))
-
+	resolvido = db.Column(db.Boolean, nullable=False, default=False)
+		# pago e não pago (despesa) | recebido e não recebido(receita)
+	data_origem = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
+	
 	json = to_json('id', 'tipo', 'valor', 'descricao', 'resolvido', \
 		'data_origem', 'id_categoria', 'id_conta_bancaria')
 
-	def __str__(self):
+	id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+	id_categoria = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
+	id_conta_bancaria = db.Column(db.Integer, db.ForeignKey('conta_bancaria.id'), nullable=False)
+
+	def do_transacao(self, conta_bancaria:ContaBancaria) -> None:
+		#TODO: Numpy
+		if self.resolvido:
+			if self.tipo == TipoTransacao.DESPESA:
+				conta_bancaria.saldo -= self.valor
+			else:
+				conta_bancaria.saldo += self.valor
+				
+			db.session.commit()
+
+	def __str__(self) -> str:
 		return f'<Transacao: id:{self.id}, tipo:{self.tipo}, valor:{self.valor}, \
 			descricao:{self.descricao}, resolvido:{self.resolvido}, data_origem:{self.data_origem}, \
 				id_categoria:{self.id_categoria}, id_conta_bancaria:{self.id_conta_bancaria}>'
 
 
-class Categoria(db.Model):
+class Categoria(BaseModelClass, db.Model):
 	__tablename__ = 'categoria'
 
 	id = db.Column(db.Integer, primary_key=True)
+	tipo = db.Column(db.Enum(TipoTransacao), nullable=False) # despesa | receita
 	nome = db.Column(db.Text, nullable=False)
 	icone = db.Column(db.Text, nullable=False)
 
-	# mercado, vestuario, saude, ...
+	json = to_json('id', 'nome', 'icone')
+
+	id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 
 	transacoes = db.relationship('Transacao', backref='categoria', lazy='select')
 
-	json = to_json('id', 'nome', 'icone')
-
-	def __str__(self):
-		return f'<Categoria: id:{self.id}, nome:{self.nome}, icone:{self.icone}>'
+	def __str__(self) -> str:
+		return f'<Categoria: id:{self.id}, tipo:{self.tipo}, nome:{self.nome}, icone:{self.icone}>'
