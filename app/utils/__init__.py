89949bdc70a6db46cb71f2d20421f0cef367566b
Lemuel Manske <lemuelkaue@gmail.com>
2022-10-30 22:42:34 -0300

    Ajustando documentação

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,27 +1,37 @@
-from http.client import BAD_REQUEST, NOT_FOUND
+from http.client import BAD_REQUEST
 
 from operator import itemgetter
 
 from flask import abort
 
-from app.models import ContaBancaria
+from app import db
 
 def get_validate(data: any, schema: 'dict[str, type]') -> 'dict[str, any]':
+	'''
+	Realiza a verificação de campos via `request.get_json()`, comparando 
+	se os campos desejados conforme schema são os mesmos que estão vindos por request.
+
+	Returns:
+		CÓD. 400 (BAD_REQUEST): Campos inválidos.
+		Sucesso: Ordenação de campos vindos por request (itemgetter).
+	'''
 
 	if type(data) != dict:
 		abort(BAD_REQUEST)
 
 	for key in schema:
 		if (key not in data) or (type(data[key]) != schema[key]):
-			abort(BAD_REQUEST) # 401
+			abort(BAD_REQUEST)
 
 	return itemgetter(*schema)(data)
 
+def do_add_commit(inst: db.Model):
+	'''
+	Realiza a adição de instancia de objeto (modelo)
+	ao banco de dados, seguido de commit.
 
-def get_conta_bancaria(b_id: int, u_id: int):
-	c = ContaBancaria.query.filter_by(id=b_id, id_usuario=u_id)
-	
-	if not c:
-		abort(NOT_FOUND)
-
-	return c
+	db.session.add(inst)
+	db.session.commit()
+	'''
+	db.session.add(inst)
+	db.session.commit()
