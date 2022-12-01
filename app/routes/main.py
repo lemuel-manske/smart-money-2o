a2bb449a8d18658f5fe6fbbc09cb39c7c50481a8
Lemuel Manske <lemuelkaue@gmail.com>
2022-11-30 22:41:13 -0300

    Página de teste de dashboard

---
+++ app/routes/main.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__, url_prefix='/app')
+
+@main.get('/')
+def render_dashboard():
+	return render_template('dashboard.html', title='Dashboard')
+
+
+@main.get('/t')
+def render_teste():
+	return render_template('teste.html', title='Teste')
