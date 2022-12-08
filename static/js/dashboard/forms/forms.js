15b08a937d63b7fdbda68c7a6e11be98861b95a9
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-08 09:29:40 -0300

    Despesa, Receita e Transferência

--- static/js/dashboard/forms/forms.js
+++ static/js/dashboard/forms/forms.js
@@ -40,8 +40,131 @@ jQuery(function($) {
 					console.log(response)
 				}
 			})
-		}		
+		}	
 	})
+
+	$('#despesa-submit').on('click', function () {
+		despesa_fields = fields.despesa;
+		
+		let [ valor, descricao ] = get_fields_values(despesa_fields.valor, despesa_fields.descricao);
+		let [ conta_bancaria, categoria ] = get_select_fields_id(despesa_fields.contas_bancarias_select,
+			despesa_fields.categorias_select);
+		let [ status ] = get_check_box_fields_values(despesa_fields.status);
+		
+		is_error = !completed_fieds(despesa_fields.valor, despesa_fields.descricao);
+
+		if (!is_error) {
+			$.ajax({
+				url: 'http://localhost:5000/api/create/transacao/despesa',
+				method: 'POST',
+				headers: {
+					'Authorization': 'Bearer ' + localStorage.getItem('x-sm-update-bearer-token')
+				},
+				contentType: 'application/json',
+				dataType: 'json',
+				data: JSON.stringify({
+					valor: valor,
+					descricao: descricao,
+					resolvido: status,
+					id_conta_bancaria: parseInt(conta_bancaria),
+					id_categoria: parseInt(categoria)
+				}),
+				
+				success: (res) => {
+					sessionStorage.setItem('flash_message', `Despesa adicionada com sucesso!`);
+					
+					location.reload();
+				},
+
+				error: (xhr) => {
+					response = xhr.responseJSON;
+					console.log(response)
+				}
+			})
+		}
+	})
+
+	$('#receita-submit').on('click', function () {
+		receita_fields = fields.receita;
+		
+		let [ valor, descricao ] = get_fields_values(receita_fields.valor, receita_fields.descricao);
+		let [ conta_bancaria, categoria ] = get_select_fields_id(receita_fields.contas_bancarias_select,
+			receita_fields.categorias_select);
+		let [ status ] = get_check_box_fields_values(receita_fields.status);
+
+		is_error = !completed_fieds(receita_fields.valor, receita_fields.descricao);
+
+		if (!is_error) {
+			$.ajax({
+				url: 'http://localhost:5000/api/create/transacao/receita',
+				method: 'POST',
+				headers: {
+					'Authorization': 'Bearer ' + localStorage.getItem('x-sm-update-bearer-token')
+				},
+				contentType: 'application/json',
+				dataType: 'json',
+				data: JSON.stringify({
+					valor: valor,
+					descricao: descricao,
+					resolvido: status,
+					id_conta_bancaria: parseInt(conta_bancaria),
+					id_categoria: parseInt(categoria)
+				}),
+				
+				success: (res) => {
+					sessionStorage.setItem('flash_message', `Receita adicionada com sucesso!`);
+					
+					location.reload();
+				},
+
+				error: (xhr) => {
+					response = xhr.responseJSON;
+					console.log(response)
+				}
+			})
+		}
+	})
+
+	$('#transferencia-submit').on('click', function () {
+		transferencia_fields = fields.transferencia;
+		
+		let [ valor ] = get_fields_values(transferencia_fields.valor, transferencia_fields.descricao);
+		let [ conta_destino, conta_origem ] = get_select_fields_id(
+			transferencia_fields.contas_bancarias_destino_select,
+			transferencia_fields.contas_bancarias_origem_select);
+
+		is_error = !completed_fieds(transferencia_fields.valor);
+
+		if (!is_error) {
+			$.ajax({
+				url: 'http://localhost:5000/api/create/transferencia',
+				method: 'POST',
+				headers: {
+					'Authorization': 'Bearer ' + localStorage.getItem('x-sm-update-bearer-token')
+				},
+				contentType: 'application/json',
+				dataType: 'json',
+				data: JSON.stringify({
+					valor: valor,
+					id_conta_bancaria_origem: parseInt(conta_origem),
+					id_conta_bancaria_destino: parseInt(conta_destino)
+				}),
+				
+				success: (res) => {
+					sessionStorage.setItem('flash_message', `Transferência adicionada com sucesso!`);
+					
+					location.reload();
+				},
+
+				error: (xhr) => {
+					response = xhr.responseJSON;
+					show_error(transferencia_fields[response.target], response.msg);
+				}
+			})
+		}
+	})
+
+
 
 	// $.ajax({
 	// 	url: 'http://localhost:5000/api/list/contas-bancarias',
