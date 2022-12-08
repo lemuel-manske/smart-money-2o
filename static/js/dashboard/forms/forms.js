d1dbf86424ad9e15002a78cc9e0ebec7cd232ed6
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-08 01:39:58 -0300

    Atualizando página principal

--- static/js/dashboard/forms/forms.js
+++ static/js/dashboard/forms/forms.js
@@ -1,33 +1,98 @@
-const fields = {
-	despesa: {
-		contas_bancarias_select: "#contas-bancarias-despesa",
-		categorias_select: "#categorias-despesa",
-		valor: "#valor-despesa",
-		status: "#status-despesa",
-		descricao: "#descricao-despesa"
-	}
-}
-
 jQuery(function($) {
+
+	clean_errors();
 	
-	cleanErrors();
+	check_flash_message();
 
-	$('#despesa-submit').on('click', function () {
-		despesa_fields = fields.despesa
+	$('#conta-bancaria-submit').on('click', function () {
+		conta_bancaria_fields = fields.conta_bancaria;
 
-		let [ contas_bancarias_select, categorias_select, valor, foi_pago, descricao ] = get_validate(
-			despesa_fields.contas_bancarias_select,
-			despesa_fields.categorias_select,
-			despesa_fields.valor,
-			despesa_fields.status,
-			despesa_fields.descricao)
+		let [ nome, saldo ] = get_fields_values(conta_bancaria_fields.nome, conta_bancaria_fields.saldo)
+		let [ moeda, instituicao ] = get_select_fields_values(conta_bancaria_fields.moeda_select, 
+				conta_bancaria_fields.instituicao_select)
 
-		is_error = !completed_fieds(despesa_fields.valor, despesa_fields.descricao)
+		is_error = !completed_fieds(conta_bancaria_fields.saldo, conta_bancaria_fields.nome)
 
 		if (!is_error) {
 			$.ajax({
-				url: 'http://localhost:5000/api/create/'
+				url: 'http://localhost:5000/api/create/conta-bancaria',
+				method: 'POST',
+				headers: {
+					'Authorization': 'Bearer ' + localStorage.getItem('x-sm-update-bearer-token')
+				},
+				contentType: 'application/json',
+				dataType: 'json',
+				data: JSON.stringify({
+					nome: nome,
+					saldo: saldo,
+					moeda: moeda,
+					instituicao: instituicao
+				}),
+				
+				success: (res) => {
+					sessionStorage.setItem('flash_message', `Conta bancária "${nome}" adicionada com sucesso!`);
+					
+					location.reload();
+				},
+
+				error: (xhr) => {
+					response = xhr.responseJSON;
+					console.log(response)
+				}
 			})
-		}
+		}		
 	})
+
+	// $.ajax({
+	// 	url: 'http://localhost:5000/api/list/contas-bancarias',
+	// 	method: 'GET',
+	// 	headers: {
+	// 		'Authorization': 'Bearer ' + localStorage.getItem('x-sm-update-bearer-token')
+	// 	},
+		
+	// 	success: (res) => {
+	// 		// Pegar somente os primeiros 4 elementos
+	// 		let contas_bancarias = (JSON.parse(
+	// 			JSON.stringify(res))).slice(0, 4)
+			
+	// 		for (conta of contas_bancarias) {
+	// 			$('#contas-bancarias-resume').append(
+	// 				criar_article_conta_bancaria(conta.id, conta.moeda, conta.instituicao, parseFloat(conta.saldo).toFixed(2))
+	// 			);
+	// 		}
+	// 	}
+	// })
+
+	// $.ajax({
+	// 	url: 'http://localhost:5000/api/list/categorias',
+	// 	method: 'GET',
+	// 	headers: {
+	// 		'Authorization': 'Bearer ' + localStorage.getItem('x-sm-update-bearer-token')
+	// 	},
+		
+	// 	success: (categorias) => {
+	// 		for (cat of categorias) {
+	// 			alert(cat)
+	// 		}
+	// 	}
+	// })
+
+	// $('#despesa-submit').on('click', function () {
+	// 	despesa_fields = fields.despesa
+
+	// 	let [ contas_bancarias_select, categorias_select, valor, foi_pago, descricao ] = get_validate(
+	// 		despesa_fields.contas_bancarias_select,
+	// 		despesa_fields.categorias_select,
+	// 		despesa_fields.valor,
+	// 		despesa_fields.status,
+	// 		despesa_fields.descricao)
+
+	// 	is_error = !completed_fieds(despesa_fields.valor, despesa_fields.descricao)
+
+	// 	if (!is_error) {
+	// 		$.ajax({
+	// 			url: 'http://localhost:5000/api/create/'
+	// 		})
+	// 	}
+	// })
 })
--- static/js/utils.js
+++ static/js/utils.js
@@ -2,13 +2,17 @@ function get_fields_values(...fields) {
 	return fields.map((id) => $(id).val());
 }
 
-function showError(field, msg) {
+function get_select_fields_values(...fields) {
+	return fields.map((id) => $(`${id} option:selected`).attr('value'));
+}
+
+function show_error(field, msg) {
 	$(field).toggleClass('is-invalid', true);
 	$(field).parent().toggleClass('is-invalid', true);
 	$(field + '-erro').text(msg);
 }
 
-function showSuperError(id, msg) {
+function show_super_error(id, msg) {
 	$(id).text(msg);
 }
 
@@ -16,18 +20,31 @@ function completed_fieds(...fields) {
 	let tmp = true;
 	for (let field of fields) {
 		if (!$(field).val()) {
-			showError(field, 'Preencha o campo.');
+			show_error(field, 'Preencha o campo.');
 			tmp = false;
 		}
 	}
 	return tmp;
 }
 
-function cleanErrors() {
+function clean_errors() {
 	$('.form-control').on('input', function() {
 		$(this).removeClass('is-invalid');
 	});
 	$('.form-floating').on('input', function() {
 		$(this).removeClass('is-invalid');
 	});
+}
+
+function check_flash_message() {
+	if (sessionStorage.getItem('flash_message')) {
+		let flash_message = sessionStorage.getItem('flash_message');
+
+		$('.toast-body').append(flash_message);
+
+		let toast = new bootstrap.Toast(document.getElementById('flash-message'))
+		toast.show()
+		
+		sessionStorage.removeItem('flash_message')
+	}
 }
