1da14130e547d64eb06900289da5a3c58ae6dc1d
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-08 01:37:45 -0300

    Atualizando rota "enum"

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,6 +1,7 @@
 from flask import jsonify, Blueprint, render_template
 
 from app.utils.choices import Moedas, Instituicoes, TipoTransacao
+from app.utils import response
 
 
 home = Blueprint('home_routes', __name__)
@@ -10,13 +11,21 @@ def home_route():
 	return render_template('home.html', title='Home')
 
 
-@home.route('/enum', methods=['GET'])
-def rota_informacoes_enum():
+@home.route('/enum/<string:classe>', methods=['GET'])
+def rota_informacoes_enum(classe: str):
 	'''
 	Retorna classes enumeradores existentes.
 	'''
-	enums = [Moedas, Instituicoes, TipoTransacao]
+	
+	resultado = None
 
-	res = [enum.repr() for enum in enums]
+	if classe == 'moedas':
+		resultado = Moedas.to_json()
+	elif classe == 'tipo-transacao':
+		resultado = TipoTransacao.to_json()
+	elif classe == 'instituicao-bancaria':
+		resultado = Instituicoes.to_json()
+	else:	
+		return response(404, 'Nenhuma classe encontrada com o nome informado.')
 
-	return jsonify(res)
+	return jsonify(resultado)
