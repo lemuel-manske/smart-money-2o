08b0ea475f0d68e2806f59827c1a34ebd07525d4
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-08 01:38:03 -0300

    Adicionando "currency_symbol"

--- app/routes/main.py
+++ app/routes/main.py
@@ -4,8 +4,7 @@ main = Blueprint('main', __name__, url_prefix='/app')
 
 @main.get('/')
 def render_dashboard():
-	return render_template('dashboard.html', title='Dashboard')
-
+	return render_template('dashboard.html', title='Dashboard', currency_symbol='R$')
 
 @main.get('/t')
 def render_teste():
