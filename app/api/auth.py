9a2b08de1c24e967fb85d45e70d25d3944f59289
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-11 10:52:10 -0300

    Removendo /atualizar-conta de auth.py

--- app/api/auth.py
+++ app/api/auth.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 
 from flask import Response, Blueprint, request
 
-from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, jwt_required, set_access_cookies, unset_jwt_cookies
+from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, set_access_cookies
 
 from app import bcrypt
 from app.utils import get_validate, response, email_validate
@@ -17,7 +17,7 @@ auth_routes = Blueprint('auth_routes', __name__, url_prefix='/api/auth')
 categorias_padroes = [
 	{
 		'tipo': 'DESPESA',
-		'icone': 'vest',
+		'icone': 'shirt',
 		'nome': 'Vestuário',
 	},
 	{
@@ -158,82 +158,6 @@ def rota_cadastro():
 	set_access_cookies(res, tk)
 
 	return res
-
-
-@auth_routes.post('/atualizar-conta')
-@jwt_required()
-def rota_atualizar_conta():
-	'''
-	Realiza a alteração dos dados do usuário via POST.
-
-	Realiza request de dados em json (email, senha, nova_senha e nome),
-	validando os mesmos (`get_validate`).
-
-	Returns:
-		CÓD. 200 (OK): Atualiza os dados e retorna objeto 
-			usuário em formato json.
-		CÓD. 400 (BAD REQUEST): Email inválido com base em regex.
-		CÓD. 401 (UNAUTHORIZED): Senha incorreta.
-	'''
-	email, senha, senha_nova, nome = get_validate(request.get_json(), \
-		{
-			'email': str,
-			'senha': str,
-			'nova_senha': str,
-			'nome': str
-		})
-	
-	usuario: Usuario = Usuario.query.filter_by(id=get_jwt_identity()).first()
-
-	if not bcrypt.check_password_hash(usuario.senha, senha):
-		response(401, 'Senha incorreta.')
-
-	if not email_validate(email):
-		response(400, 'Email inválido')
-
-	nova_senha_hash = bcrypt.generate_password_hash(senha_nova).decode('UTF-8')
-
-	usuario.update(
-		email=email, 
-		senha=nova_senha_hash,
-		nome=nome
-	)
-
-	res = response(200, usuario.json())
-
-	return res
-
-
-@auth_routes.get('/logout')
-@jwt_required()
-def rota_logout():
-	'''
-	Realiza o logout do usuário.
-
-	Returns:
-		CÓD. 200 (OK): JWT excluída.
-	'''
-	resp = response(200, 'Logout bem sucedido')
-	unset_jwt_cookies(resp)
-
-	return resp
-
-
-@auth_routes.get('/minha-conta')
-@jwt_required()
-def rota_minha_conta():
-	'''
-	Retorna as informações do usuário
-
-	Returns:
-		CÓD. 200 (OK): Objeto json com informações 
-			da conta do usuário
-	'''
-	usuario: Usuario = Usuario.query.filter_by(id=get_jwt_identity()).first()
-
-	resp = response(200, usuario.json())
-
-	return resp
 
 
 @auth_routes.after_app_request
