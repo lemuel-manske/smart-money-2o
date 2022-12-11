d245a2d59cb864ffc5b76a7ad39ccf7b1b49dd2c
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-11 17:06:46 -0300

    Frontend deletar e realizar transações

--- static/js/historico/forms.js
+++ static/js/historico/forms.js
@@ -1,9 +1,61 @@
 jQuery(function($) {
 	$(document.body).on('click', '.excluir-transacao', function () {
 		let transacao_id = $(this).closest('tr').find('th').attr('id');
-		
-		console.log(transacao_id)
 
-		// ajax
+		$.ajax({
+			url: 'http://localhost:5000/api/delete/transacao',
+			method: 'DELETE',
+			xhrFields: {
+				withCredentials: true
+			},
+			headers: {
+				'X-CSRF-TOKEN':getCookie('csrf_access_token')
+			},
+
+			contentType: 'application/json',
+			dataType: 'json',
+			data: JSON.stringify({
+				id_transacao: parseInt(transacao_id)
+			}),
+
+			success: (res) => {
+				location.reload()
+			},
+
+			error: (xhr) => {
+				response = xhr.responseJSON;
+				console.log(response);
+			}
+		})
+	})
+
+	$(document.body).on('click', '.efetuar-resolver', function () {
+		let transacao_id = $(this).closest('tr').find('th').attr('id');
+
+		$.ajax({
+			url: 'http://localhost:5000/api/create/transacao/realizar',
+			method: 'POST',
+			xhrFields: {
+				withCredentials: true
+			},
+			headers: {
+				'X-CSRF-TOKEN':getCookie('csrf_access_token')
+			},
+
+			contentType: 'application/json',
+			dataType: 'json',
+			data: JSON.stringify({
+				id_transacao: parseInt(transacao_id)
+			}),
+
+			success: (res) => {
+				location.reload()
+			},
+
+			error: (xhr) => {
+				response = xhr.responseJSON;
+				console.log(response);
+			}
+		})
 	})
 })
