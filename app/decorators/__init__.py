8f07002bfc48266b0a05abeda02aef436b08085e
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-11 10:53:06 -0300

    Decorados de checagem JWT nos cookies

--- app/decorators/__init__.py
+++ app/decorators/__init__.py
@@ -1,12 +1,21 @@
 from functools import wraps
 
 from flask import redirect, url_for
-from flask_jwt_extended import jwt_required, verify_jwt_in_request
+from flask_jwt_extended import verify_jwt_in_request
 
 from app.models import Usuario
 
 
-def requer_login(route):
+def check_for_jwt(route):
+	'''
+	Realiza verificação de JWT no request,
+	com `optional=True`, isso significa que 
+	caso uma JWT seja encontrada o retorno é True,
+	caso contrário o retorno é None.
+
+	Caso nenhuma JWT seja encontrada, a função
+	redireciona o usuário para a página de login.
+	'''
 	@wraps(route)
 	def wrapper(*args, **kwargs):
 		if verify_jwt_in_request(optional=True) == None:
