7da8d1b046d18f4a004eaf773ba425b06738c2d0
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-11 10:58:22 -0300

    Update popular_dashboard.js

--- static/js/dashboard/popular_dashboard.js
+++ static/js/dashboard/popular_dashboard.js
@@ -1,76 +1,41 @@
-let criar_article_conta_bancaria = (id, moeda, instituicao, saldo) => {
-	return `<article class="col" data-id-conta-bancaria="${id}">
+let criar_article_conta_bancaria = (conta_bancaria) => {
+	return `<article class="col">
 		<div class="card rounded-4 shadow">
 			<div class="d-flex flex-column px-5 pt-4 pb-3">
-				<div class="text-center pb-3 pt-2">
-					<svg height="5em" width="5em">
-						<use xlink:href="#banco" />
+				<div class="d-flex align-items-center pb-3 pt-2">
+					<svg class="pe-3" height="4em" width="4em">
+						<use xlink:href="#bank" />
 					</svg>
+					<h3>${conta_bancaria.nome}</h3>
 				</div>
+				<h5>${conta_bancaria.instituicao}</h5>
 
-				<h3 class="d-flex">
-					${moeda}
-					<p class="ms-2" id="saldo">${saldo}</p>
+				<h3 class="d-flex mb-2">
+					${conta_bancaria.usuario.moeda}
+					<p class="ms-2" id="saldo">${parseFloat(conta_bancaria.saldo).toFixed(2)}</p>
 				</h3>
-				<h5>${instituicao}</h5>
 
-
-				<table class="table" id="tabela-conta-bancaria-1">
+				<h5>Suas transações</h5>
+				
+				<table class="table" id="tabela-conta-bancaria-${conta_bancaria.id}">
 					<thead>
 						<tr>
-							<th scope="col">#</th>
+							<th scope="col"></th>
 							<th scope="col">Valor</th>
+							<th scope="col">Tipo</th>
 							<th scope="col">Categoria</th>
-							<th scope="col">Conta bancária</th>
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<th scope="row">1</th>
-							<td>Mark</td>
-							<td>Otto</td>
-							<td>@mdo</td>
+						<tr id="tabela-conta-bancaria-${conta_bancaria.id}-default-row">
+							<th scope='row'>...</th>
+							<td>...</td>
+							<td>...</td>
+							<td>...</td>
 						</tr>
 					</tbody>
 				</table>
 			</div>
 		</div>
 	</article>
-`}
-
-let criar_article_transacoes = (cor_hex, ) => {
-	return `<article class="col">
-	<div class="card rounded-4 shadow">
-		<div class="d-flex flex-column px-5 py-5 pb-3">
-			<div class="d-flex align-items-center pb-2">
-				<svg fill="#198754" class="me-3" height="2em" width="2em">
-					<use xlink:href="#arrow-up" />
-				</svg>
-				<h3 class="mb-0 fw-bold">RECEITAS</h3>
-			</div>
-			<h3 class="d-flex">
-				{{ currency_symbol }}
-				<p class="ms-2" id="valor-receitas">00,00</p>
-			</h3>
-			<table class="table" id="tabela-receitas">
-				<thead>
-					<tr>
-						<th scope="col"></th>
-						<th scope="col">Valor</th>
-						<th scope="col">Categoria</th>
-						<th scope="col">Conta bancária</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-</article>
 `}
