c6561ee239318e747d8b995989a4888976c4fed8
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-11 10:56:19 -0300

    Auth.js -> User.js

--- static/js/user/user.js
+++ static/js/user/user.js
@@ -1,32 +1,30 @@
-const fields = {
-	login: {
-		email: '#email-login',
-		senha: '#senha-login'
-	},
-	cadastro: {
-		email: '#email-cadastro',
-		nome: '#nome-cadastro',
-		senha: '#senha-cadastro'
-	}
+const LOGIN_FIELDS = {
+	email: '#email-login',
+	senha: '#senha-login'
+}
+
+const CADASTRO_FIELDS = {
+	email: '#email-cadastro',
+	nome: '#nome-cadastro',
+	senha: '#senha-cadastro'
 }
 
 jQuery(function($) {
 
-	clean_errors();
+	remover_erros();
 
 	$('#login-submit').on('click', function() {
-		login_fields = fields.login;
-
-		let [ email, senha ] = get_fields_values(login_fields.email, login_fields.senha)
+		let [ email, senha ] = get_fields_values(LOGIN_FIELDS.email, LOGIN_FIELDS.senha)
 
 		is_error = !completed_fieds(
-			login_fields.email,
-			login_fields.senha)
+			LOGIN_FIELDS.email,
+			LOGIN_FIELDS.senha)
 		
 		if (!is_error) {
 			$.ajax({
 				url: 'http://localhost:5000/api/auth/login',
 				method: 'POST',
+				
 				contentType: 'application/json',
 				dataType: 'json',
 				data: JSON.stringify({
@@ -35,35 +33,33 @@ jQuery(function($) {
 				}),
 				
 				success: (res) => {
-					token = res.msg;
-					localStorage.setItem('x-sm-update-bearer-token', token);
-				
+					set_user_moeda()
+
 					location.pathname = '/app/';
 				},
 
 				error: (xhr) => {
 					response = xhr.responseJSON;
-					show_error(login_fields[response.target], response.msg);
+					show_error(LOGIN_FIELDS[response.target], response.msg);
 				}
 			})
 		}
 	})
 
 	$('#cadastro-submit').on('click', function () {
-		cadastro_fields = fields.cadastro;
-
-		let [ email, nome, senha ] = get_fields_values(cadastro_fields.email, cadastro_fields.nome, cadastro_fields.senha)
+		let [ email, nome, senha ] = get_fields_values(CADASTRO_FIELDS.email, CADASTRO_FIELDS.nome, CADASTRO_FIELDS.senha)
 
 		let is_error = !completed_fieds(
-			cadastro_fields.email,
-			cadastro_fields.nome,
-			cadastro_fields.senha
+			CADASTRO_FIELDS.email,
+			CADASTRO_FIELDS.nome,
+			CADASTRO_FIELDS.senha
 		)
 
 		if (!is_error) {
 			$.ajax({
 				url: 'http://localhost:5000/api/auth/cadastro',
 				method: 'POST',
+				
 				contentType: 'application/JSON',
 				dataType: 'json',
 				data: JSON.stringify({
@@ -73,14 +69,14 @@ jQuery(function($) {
 				}),
 
 				success: (res) => {
-					token = res.msg;
-					sessionStorage.setItem('x-sm-update-bearer-token', token);
+					set_user_moeda()
+
 					location.pathname = '/app/';
 				},
 				
 				error: (xhr) => {
 					response = xhr.responseJSON;
-					show_error(cadastro_fields[response.target], response.msg);
+					show_error(CADASTRO_FIELDS[response.target], response.msg);
 				}
 			})
 		}
