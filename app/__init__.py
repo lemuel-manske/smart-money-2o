9d5fa6e228715346b76ef303db7808dbc0cc087d
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-11 10:51:38 -0300

    Removendo CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
@@ -11,10 +10,6 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
-# TOKEN_UPDATE_HEADER = 'X-SM-Update-Bearer-Token'
-
-# cors = CORS(expose_headers=[TOKEN_UPDATE_HEADER], allow_headers=['Authorization'], resources={r"*": {"origin": "*"}})
-
 def create_app(config=Config):
 	app = Flask('SmartMoney')
 	app.config.from_object(Config)
@@ -22,12 +17,12 @@ def create_app(config=Config):
 	db.init_app(app)
 	bcrypt.init_app(app)
 	jwt.init_app(app)
-	# cors.init_app(app)
 
 	with app.app_context():
 		db.session.execute('PRAGMA FOREIGN_KEYS=ON')
 
 	from app.api.auth import auth_routes
+	from app.api.user import user_routes
 	from app.api.create import create_routes
 	from app.api.list import list_routes
 
@@ -36,6 +31,7 @@ def create_app(config=Config):
 	from app.routes.main import main
 
 	app.register_blueprint(auth_routes)
+	app.register_blueprint(user_routes)
 	app.register_blueprint(create_routes)
 	app.register_blueprint(list_routes)
 
