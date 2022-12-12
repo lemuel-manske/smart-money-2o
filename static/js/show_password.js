4d8a75f158e2c8ebe51ba5fc0e977edc79454d20
Lemuel Manske <lemuelkaue@gmail.com>
2022-12-11 23:27:09 -0300

    Update show_password.js

--- static/js/show_password.js
+++ static/js/show_password.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
 	$(".toggle-visibility-button").on("click", function() {
 		let type = ($(this).is(':checked') ? 'text' : 'password');
-		let campo = $(this).parent().parent().parent().find('.toggle-visibility');
+		let campo = $(this).closest('.show-password-group').find('.toggle-visibility');
 
 		campo.attr('type', type);
 	});
